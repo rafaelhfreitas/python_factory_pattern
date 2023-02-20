3951f1dacccae11b86d3360c38887e9d56239fd5
Rafael Freitas <rafael@ezsolucoes.com.br>
2023-02-20 11:05:49 -0300

    Add a new factory pattern to control service registration of music apps

---
+++ example3/program.py
@@ -0,0 +1,24 @@
+import music
+
+config = {
+    'spotify_client_key': 'THE_SPOTIFY_CLIENT_KEY',
+    'spotify_client_secret': 'THE_SPOTIFY_CLIENT_SECRET',
+    'pandora_client_key': 'THE_PANDORA_CLIENT_KEY',
+    'pandora_client_secret': 'THE_PANDORA_CLIENT_SECRET',
+    'local_music_location': '/usr/data/music'
+}
+
+pandora = music.services.create('PANDORA', **config)
+pandora.test_connection()
+
+spotify = music.services.create('SPOTIFY', **config)
+spotify.test_connection()
+
+local = music.services.create('LOCAL', **config)
+local.test_connection()
+
+pandora2 = music.services.get('PANDORA', **config)
+print(f'id(pandora) == id(pandora2): {id(pandora) == id(pandora2)}')
+
+spotify2 = music.services.get('SPOTIFY', **config)
+print(f'id(spotify) == id(spotify2): {id(spotify) == id(spotify2)}')    
